d00f29ed984cdead4147660e9b4667783bbbb521
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-16 23:35:52 -0300

    Use env

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.SERVER}
+          {process.env.REACT_APP_SERVER}
         </p>
         <a
           className="App-link"
